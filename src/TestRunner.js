58f9e24aea14307531c3df88615f252c59bad879
kangasaaron <kangasaaron@users.noreply.github.com>
2021-01-15 19:39:41 -0800

    Add files via upload

---
+++ src/TestRunner.js
@@ -0,0 +1,12 @@
+import "./other/EnumTest.js";
+import "./other/SerializableTest.js";
+import "./Map/Coordinates/AxisTest.js";
+import "./Game/GenderTest.js";
+import "./Core/UtilsTest.js"
+import "./Core/DiceTest.js"
+import "./Data/DataBaseTest.js"
+
+// TODO
+// import "./Job/LoiterJobTest.js"
+// import "./Data/DataLibraryTest.js"
+import "./Core/ClockTest.js"
